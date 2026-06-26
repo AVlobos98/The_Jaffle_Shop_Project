12fa6977c668621d0dac20f2748ed0a0efe14255
Antonio Villalobos <avlobos98@gmail.com>
2026-06-26 18:36:54 +0000

    add store join to int_order_summary

--- models/Intermediate/int_order_summary.sql
+++ models/Intermediate/int_order_summary.sql
@@ -6,13 +6,21 @@ customers AS (
     SELECT * FROM {{ ref('stg_jaffle_shop__customers') }}
 ),
 
+stores AS ( 
+    SELECT * FROM {{ ref('stg_jaffle_shop__stores') }}
+),
+
+
 final AS (
     SELECT 
         o.*,
-        c.customer_name
+        c.customer_name,
+        s.store_name
     FROM orders o
     LEFT JOIN customers c 
         ON o.customer_id = c.customer_id
+    LEFT JOIN stores s 
+        ON o.store_id = s.store_id
 
 )
 
